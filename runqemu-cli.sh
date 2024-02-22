c2bcbe9147cd4c94e7870a86ade74127b19ce171
Daniel Wieczorek <121404035+byte4RR4Y@users.noreply.github.com>
2024-02-22 19:22:12 +0100

    Update runqemu-cli.sh

--- runqemu-cli.sh
+++ runqemu-cli.sh
@@ -11,17 +11,7 @@ RW=$2
 CPUS=$(nproc) 
 CPUS=$((CPUS > 8 ? 8 : CPUS))
 
-if [ "$RW" == "" ]; then
-  qemu-system-aarch64 -M virt -m 2048 -cpu cortex-a72 -smp $CPUS \
-    -kernel "$(dirname "$SDCARD")/.qemu/"vmlinuz \
-    -initrd "$(dirname "$SDCARD")/.qemu/"initrd.img \
-    -drive if=none,file=${SDCARD},format=raw,id=hd \
-    -device virtio-blk-pci,drive=hd \
-    -netdev user,id=mynet \
-    -nographic \
-    -bios /usr/lib/u-boot/qemu_arm64/u-boot.bin \
-    -append "root=LABEL=rootfs ro"
-elif [ "$RW" == "rw" ]; then
+if [ "$RW" == "rw" ]; then
   qemu-system-aarch64 -M virt -m 2048 -cpu cortex-a72 -smp $CPUS \
     -kernel "$(dirname "$SDCARD")/.qemu/"vmlinuz \
     -initrd "$(dirname "$SDCARD")/.qemu/"initrd.img \
@@ -31,4 +21,14 @@ elif [ "$RW" == "rw" ]; then
     -nographic \
     -bios /usr/lib/u-boot/qemu_arm64/u-boot.bin \
     -append "root=LABEL=rootfs rw"
+else
+   qemu-system-aarch64 -M virt -m 2048 -cpu cortex-a72 -smp $CPUS \
+    -kernel "$(dirname "$SDCARD")/.qemu/"vmlinuz \
+    -initrd "$(dirname "$SDCARD")/.qemu/"initrd.img \
+    -drive if=none,file=${SDCARD},format=raw,id=hd \
+    -device virtio-blk-pci,drive=hd \
+    -netdev user,id=mynet \
+    -nographic \
+    -bios /usr/lib/u-boot/qemu_arm64/u-boot.bin \
+    -append "root=LABEL=rootfs ro"
 fi
