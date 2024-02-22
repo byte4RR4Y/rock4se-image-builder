959ec38e516cc259bbfe4bc5abd37478392b1ad5
Daniel Wieczorek <121404035+byte4RR4Y@users.noreply.github.com>
2024-02-22 11:59:05 +0100

    Add files via upload

--- runqemu.sh
+++ runqemu.sh
@@ -10,14 +10,11 @@ SDCARD=$1
 CPUS=$(nproc) # Ermittelt die Anzahl der CPUs des Hostsystems
 CPUS=$((CPUS > 8 ? 8 : CPUS)) # Begrenzt die Anzahl der CPUs auf maximal 8
 
-# Wechseln zum .qemu-Verzeichnis
-cd "$(dirname "$SDCARD")/.qemu" || exit 1
-
 # Ausführen des QEMU-Befehls
 qemu-system-aarch64 -M virt -m 1024 -cpu cortex-a53 -smp $CPUS \
-  -kernel vmlinuz \
-  -initrd initrd.img \
-  -drive if=none,file=disk.raw,format=raw,id=hd \
+  -kernel "$(dirname "$SDCARD")/.qemu/"vmlinuz \
+  -initrd "$(dirname "$SDCARD")/.qemu/"initrd.img \
+  -drive if=none,file=${SDCARD},format=raw,id=hd \
   -device virtio-blk-pci,drive=hd \
   -netdev user,id=mynet \
   -device virtio-net-pci,netdev=mynet \
