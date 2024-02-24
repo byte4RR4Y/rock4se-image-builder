65e91c6b71ae250e83956806f88055f5d0fe4661
Daniel Wieczorek <121404035+byte4RR4Y@users.noreply.github.com>
2024-02-24 12:59:15 +0100

    Add files via upload

--- build.sh
+++ build.sh
@@ -244,24 +244,7 @@ if [[ "$BUILD" == "yes" ]]; then
     if [ "$DESKTOP" != "CLI" ]; then
         CPUS=$(nproc) 
         CPUS=$((CPUS > 8 ? 8 : CPUS))
-
-        qemu-system-aarch64 \
-        -M virt \
-        -cpu cortex-a76 \
-        -m 2048 \
-        -smp $CPUS \
-        -kernel output/Debian-${SUITE}-${DESKTOP}-build-${TIMESTAMP}/.qemu/"vmlinuz \
-        -initrd output/Debian-${SUITE}-${DESKTOP}-build-${TIMESTAMP}/.qemu/"initrd.img \
-        -append "console=ttyAMA0 root=/dev/vda rw" \
-        -drive if=none,file="output/Debian-${SUITE}-${DESKTOP}-build-${TIMESTAMP}/Debian-${SUITE}-${DESKTOP}-build.img",format=raw,id=disk \
-        -bios /usr/lib/u-boot/qemu_arm64/u-boot.bin \
-        -append "root=LABEL=rootfs rw" \
-        -device virtio-blk-device,drive=disk \
-        -device virtio-keyboard-pci \
-        -netdev user,id=net0 \
-        -device virtio-net-pci,netdev=net0 \
-        -device qemu-xhci -display gtk,gl=on,show-cursor=on \
-        -device virtio-gpu-pci -device virtio-mouse-pci
+        ./runqemu-desktop.sh "output/Debian-${SUITE}-${DESKTOP}-build-${TIMESTAMP}/Debian-${SUITE}-${DESKTOP}-build.img"
     fi
 fi
 
