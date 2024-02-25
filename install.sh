40383192ee3e68ad63f2b8766b8867d85a805c3d
Daniel Wieczorek <121404035+byte4RR4Y@users.noreply.github.com>
2024-02-25 18:27:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ if [ "$UID" -ne 0 ]; then
     exit 1
 fi
 
-apt update -y && apt install -y git docker.io apparmor qemu-user-static binfmt-support qemu-user qemu-utils qemu-system-aarch64 u-boot-qemu
+apt update -y && apt install -y git docker.io apparmor qemu-user-static binfmt-support qemu-user qemu-utils qemu-system-aarch64 u-boot-qemu golang
+go mod init Image-builder-GUI_amd64.go
+go mod tidy
+GOOS=linux GOARCH=amd64 go build Image-builder-GUI_amd64 Image-builder-GUI_amd64.go
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
