1ed11c807df7d93e3335b1c545fa87bdb28e0508
Daniel Wieczorek <121404035+byte4RR4Y@users.noreply.github.com>
2024-02-28 20:15:14 +0100

    Delete config/installkernel.sh

--- config/installkernel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ARCHIVE=$1
-
-BUILD="$(unzip -l ${ARCHIVE} boot/vmlinuz-* | sed -n 's|^.*boot/vmlinuz-\(.*\)$|\1|p')"
-unzip -o "${ARCHIVE}" -d /
-update-initramfs -c -v -k "${BUILD}"
-
