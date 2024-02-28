149bfffa7037bf388de44b580fc383ef6047e5a8
Daniel Wieczorek <121404035+byte4RR4Y@users.noreply.github.com>
2024-02-28 05:31:36 +0100

    Add files via upload

--- config/makekernel.sh
+++ config/makekernel.sh
@@ -3,12 +3,7 @@
 CWD=$PWD
 OUTDIR=${CWD}
 CPUS=$(($(nproc)))
-if [ "$ARG" == "1" ]; then
-git clone --depth=1 https://github.com/torvalds/linux
-cd linux
 CONFIG_FILE="arch/arm64/configs/defconfig"
-
-# Liste der Konfigurationsvariablen, die auf 'n' gesetzt werden sollen
 CONFIG_VARS=(
     CONFIG_ARCH_ACTIONS
     CONFIG_ARCH_SUNXI
@@ -54,46 +49,47 @@ CONFIG_VARS=(
     CONFIG_ARCH_ZYNQMP
 )
 
-# Öffne das Konfigurationsfile und setze die gewünschten Variablen auf 'n'
+git clone --depth=1 https://github.com/torvalds/linux
+cd linux
+
 for var in "${CONFIG_VARS[@]}"; do
     sed -i "s/^${var}=y$/${var}=n/" "$CONFIG_FILE"
 done
+
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig
 
-KERNELDIR="KERNEL-${BRANCH}"
+BUILD="$(sed -n 's|^.*\s\+\(\S\+\.\S\+\.\S\+\)\s\+Kernel Configuration$|\1|p' .config)"
+KERNELDIR="KERNEL-${BUILD}"
 mkdir -p "${KERNELDIR}"
 
-sed -i "s/^CONFIG_LOCALVERSION=.*$/CONFIG_LOCALVERSION="-byte4rr4y"/" ".config"
-BUILD="$(sed -n 's|^.*\s\+\(\S\+\.\S\+\.\S\+\)\s\+Kernel Configuration$|\1|p' .config)"
-
-      make -j ${CPUS} KERNELRELEASE="${BUILD}" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image.gz modules dtbs
-      env PATH=$PATH make KERNELRELEASE="${BUILD}" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${KERNELDIR} modules_install
-      mkdir -p "${KERNELDIR}/boot/" "${KERNELDIR}/lib/linux-image-${BUILD}/rockchip/"
-      echo "ffffffffffffffff B The real System.map is in the linux-image-<version>-dbg package" > "${KERNELDIR}/boot/System.map-${BUILD}"
-      cp .config "${KERNELDIR}/boot/config-${BUILD}"
-      cp arch/arm64/boot/Image.gz "${KERNELDIR}/boot/vmlinuz-${BUILD}"
-      cp arch/arm64/boot/dts/rockchip/*.dtb "${KERNELDIR}/lib/linux-image-${BUILD}/rockchip/"
+make -j ${CPUS} KERNELRELEASE="${BUILD}" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image.gz modules dtbs
+env PATH=$PATH make KERNELRELEASE="${BUILD}" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${KERNELDIR} modules_install
+mkdir -p "${KERNELDIR}/boot/" "${KERNELDIR}/lib/linux-image-${BUILD}/rockchip/"
+echo "ffffffffffffffff B The real System.map is in the linux-image-<version>-dbg package" > "${KERNELDIR}/boot/System.map-${BUILD}"
+cp .config "${KERNELDIR}/boot/config-${BUILD}"
+cp arch/arm64/boot/Image.gz "${KERNELDIR}/boot/vmlinuz-${BUILD}"
+cp arch/arm64/boot/dts/rockchip/*.dtb "${KERNELDIR}/lib/linux-image-${BUILD}/rockchip/"
    
-  ARCHIVE="kernel-$(sed -n 's|^.*\s\+\(\S\+\.\S\+\.\S\+\)\s\+Kernel Configuration$|\1|p' .config)$(sed -n 's|^CONFIG_LOCALVERSION=\"\(.*\)\"$|\1|p' .config).zip"
-  cd "${KERNELDIR}"
-  find lib -type l -exec rm {} \;
-  zip -q -r "${ARCHIVE}" *
-  if [ "${OUTDIR}" != "" ]; then
-    if [ "${OUTDIR: -1}" != "/" ]; then
+ARCHIVE="kernel-$(sed -n 's|^.*\s\+\(\S\+\.\S\+\.\S\+\)\s\+Kernel Configuration$|\1|p' .config)$(sed -n 's|^CONFIG_LOCALVERSION=\"\(.*\)\"$|\1|p' .config).zip"
+cd "${KERNELDIR}"
+find lib -type l -exec rm {} \;
+zip -q -r "${ARCHIVE}" *
+if [ "${OUTDIR}" != "" ]; then
+  if [ "${OUTDIR: -1}" != "/" ]; then
       OUTDIR+="/"
-    fi
-  else
-    if [ "${REALUSER}" = "root" ]; then
-      OUTDIR="/root/"
-    else
-      OUTDIR="/home/${REALUSER}/"
-    fi
   fi
+else
+  if [ "${REALUSER}" = "root" ]; then
+      OUTDIR="/root/"
+  else
+      OUTDIR="/home/${REALUSER}/"
+  fi
+fi
 chown "${REALUSER}:${REALUSER}" "${ARCHIVE}"
 cd ${CWD}/linux
 mv "${KERNELDIR}/${ARCHIVE}" "${OUTDIR}"
 rm -rf "${KERNELDIR}"
 cd ${CWD}
 rm -rf linux
-fi
+
 echo "1" > ${CWD}/config/kernel_status
