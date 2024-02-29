b8e33fadf987ceafc367322549f1f5e169e0cbe6
Daniel Wieczorek <121404035+byte4RR4Y@users.noreply.github.com>
2024-02-29 18:20:53 +0100

    Add files via upload

--- config/kernelupdater.sh
+++ config/kernelupdater.sh
@@ -14,10 +14,12 @@ git clone --depth=1 https://github.com/torvalds/linux
 cd linux
 make ARCH=arm64 defconfig
 BUILD="$(sed -n 's|^.*\s\+\(\S\+\.\S\+\.\S\+\)\s\+Kernel Configuration$|\1|p' .config)"
-make -j ${CPUS} KERNELRELEASE="${BUILD}" ARCH=arm64 CONFIG_BRCMUTIL=m CONFIG_BRCMFMAC=m CONFIG_ARCH_ROCKCHIP=y Image.gz modules dtbs
+make -j ${CPUS} KERNELRELEASE="${BUILD}" ARCH=arm64 CONFIG_WIRELESS=y CONFIG_BRCMUTIL=m CONFIG_BRCMFMAC=m CONFIG_ARCH_ROCKCHIP=y Image.gz modules dtbs
 make modules_install
 mkdir -p "/lib/linux-image-${BUILD}/rockchip/"
 cp .config "/boot/config-${BUILD}"
 cp arch/arm64/boot/Image.gz "/boot/vmlinuz-${BUILD}"
 cp arch/arm64/boot/dts/rockchip/*.dtb "/lib/linux-image-${BUILD}/rockchip/"
+update-initramfs -c -v -k ${BUILD}
+u-boot-update
 
