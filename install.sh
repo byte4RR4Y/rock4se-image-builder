e65e66bf4a150081ac5c030de06e55afaffa728c
Daniel Wieczorek <121404035+byte4RR4Y@users.noreply.github.com>
2024-03-04 20:50:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,10 @@ if [ "$UID" -ne 0 ]; then
     exit 1
 fi
 
-apt update -y && apt install -y git bc bison flex libssl-dev make libc6-dev libncurses5-dev crossbuild-essential-arm64 docker.io apparmor qemu-user-static binfmt-support qemu-user qemu-system qemu-utils xfce4-terminal qemu-system-aarch64 u-boot-qemu golang swtpm qemu-system-gui
-go mod init Image-builder-GUI_amd64.go
-go mod tidy
+apt update -y && apt install -y git bc bison flex libssl-dev make libc6-dev libncurses5-dev crossbuild-essential-arm64 docker.io apparmor binfmt-support xfce4-terminal
 chmod +x ./*
 chmod +x config/*
-GOOS=linux GOARCH=amd64 go build -o Image-builder-GUI_amd64 Image-builder-GUI_amd64.go
+
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
  
